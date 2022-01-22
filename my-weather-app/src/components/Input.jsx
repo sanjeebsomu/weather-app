38261d98dab5455ab06a7b9d66c7e63762d1f8d4
sanjeebsomu <sanjeebgochhayat1999@gmail.com>
2022-01-22 09:56:31 +0530

    Added Functionality for Input

--- my-weather-app/src/components/Input.jsx
+++ my-weather-app/src/components/Input.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 
 const Input = () => {
+  const [cityName, setCityName] = useState('');
+  const [newCityName, setnNewCityName] = useState('');
+  const handleChange = (e) => {
+    setnNewCityName(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCityName(newCityName)
+    console.log(cityName);
+  }
   return <>
-    <div className='flex justify-center'>
-          <input className='w-1/4 h-12 mt-12 outline-none text-3xl bg-transparent' type="text" name="cityName" id="cityName"/>
-          <div className='mt-12 opacity-75'>
-          <ArrowCircleRightIcon sx={{fontSize: 40}}/>
-          </div>
-    </div>
+      <form action="" onSubmit={handleSubmit} className='flex justify-center'>
+          <input className='w-1/4 h-12 mt-12 outline-none text-3xl' type="text" name="cityName" id="cityName" value={newCityName} onChange={handleChange}/>
+          <button type="submit" className='mt-12 opacity-75'>
+            <ArrowCircleRightIcon sx={{fontSize: 50}}/>
+          </button>         
+      </form>
   </>;
 };
 
